82c34da36a3c65991e3466e5ebaf449094eb3057
Dmitry Ovsyanko <do_@pochta.ru>
2024-08-12 10:45:08 +0300

    +Tracker.LOGGING_DETAILS

---
+++ __tests__/details.js
@@ -0,0 +1,81 @@
+const {createLogger, format, transports, level, stream} = require ('winston')
+const EventEmitter = require ('events')
+const {Tracker} = require ('..')
+const {Writable} = require ('stream')
+
+class MyClass extends EventEmitter {
+
+	constructor (o = {}) {
+		super ()
+		for (const k in o) this [k] = o [k]
+	}
+
+	get [Tracker.LOGGING_ID] () {
+		return this.id
+	}
+
+	get [Tracker.LOGGING_PARENT] () {
+		return this.parent
+	}
+
+	get [Tracker.LOGGING_DETAILS] () {
+		return {
+			id: this.id
+		}
+	}
+
+}
+
+class MySubClass extends MyClass {
+
+	get [Tracker.LOGGING_DETAILS] () {
+		return {
+			...super [Tracker.LOGGING_DETAILS],
+			flag: 'A'
+		}
+	}
+
+}
+
+test ('details', () => {
+
+	let s = ''
+
+	const stream = new Writable ({
+		write (r, _, cb) {
+			s += r.toString ()
+			cb ()
+		}
+		
+	})
+
+	const logger = createLogger ({
+		transports: [
+//			new transports.Console (),
+			new transports.Stream ({stream}),
+		],
+		format: format.printf (({message, details}) => `${details.flag}${message}${details.id}`)
+	})
+	
+	const emitter = new MySubClass ({
+		id: 1,
+		parent: new MyClass ({id: 'root'})
+	})
+
+	const tracker = new Tracker (emitter, logger, {
+		events: {
+			progress: {
+				level: 'info',
+				message: v => v,
+				details: {}
+			},
+		}
+	})
+
+	tracker.listen ()
+
+	emitter.emit ('progress', 50)
+
+	expect (s.trim ()).toBe ('A501')
+
+})

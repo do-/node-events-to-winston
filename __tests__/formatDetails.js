45d53db8727664f6f57c56f86424a154902196cf
Dmitry Ovsyanko <do_@pochta.ru>
2025-05-14 11:20:29 +0300

    formatDetails +maxLength

--- __tests__/formatDetails.js
+++ __tests__/formatDetails.js
@@ -10,5 +10,12 @@ test ('basic', async () => {
 	expect (formatDetails ({mask: ['login', /^passw/]}).transform ({message: '<', details: {id: 1, auth:{login: 'a', password: '1'}}}).message).toBe ('< {"auth":{},"id":1}')
 	expect (formatDetails ({mask: {value: 'XXX'}}).transform ({message: '<', details: {id: 1, Login: 'a', Password: '1'}}).message).toBe ('< {"Login":"a","Password":"XXX","id":1}')
 	expect (formatDetails ({replacer: null}).transform ({message: '<', details: {id: 1, login: 'a', password: '1'}}).message).toBe ('< {"id":1,"login":"a","password":"1"}')
+	expect (formatDetails ({maxLength: 3, replacer: null}).transform ({message: '<', details: {id: 1, code: 'RED', label: 'Loooong'}}).message).toBe ('< {"code":"RED","id":1,"label":"Loo..."}')
+	expect (formatDetails ({maxLength: 3, replacer: (_, v) => typeof v === 'number' ? String (v) : v}).transform ({message: '<', details: {id: 1, code: 'RED', label: 'Loooong'}}).message).toBe ('< {"code":"RED","id":"1","label":"Loo..."}')
+
+	expect (() => formatDetails ({maxLength: -1})).toThrow ('maxLength')
+	expect (() => formatDetails ({maxLength: 0})).toThrow ('maxLength')
+	expect (() => formatDetails ({maxLength: Infinity})).toThrow ('maxLength')
+	expect (() => formatDetails ({maxLength: '20'})).toThrow ('maxLength')
 
 })
